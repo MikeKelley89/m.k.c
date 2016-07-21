9f6a0a95c902844e34f104920ce836de4fbe5149
MikeKelley89 <mikekelley89@gmail.com>
2016-07-21 14:04:16 -0600

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -180,13 +180,6 @@ $.fn.qtip.defaults = $.extend(true, {}, $.fn.qtip.defaults, {
 				
             $('#cssmap_tab_container').append(map_link_properties);
             
-            var thisCircle = getRadioVal( document.getElementById('radio_' + $new_id), 'circle' );
-            if (thisCircle === "yes") {
-            	$(map_link_properties).css({'border-radius': '100%'})
-            } else {
-            	$(map_link_properties).css({'border-radius': '0'})
-            }
-			
             $('#' + $new_id + '_properties').dialog({
                 autoOpen: true,
                 modal: true
