6926181051101a9504881a8308fd2b4d8e4ab9bf
MikeKelley89 <mikekelley89@gmail.com>
2017-04-19 22:13:16 -0600

    moar testiong

--- js/functions.js
+++ js/functions.js
@@ -30,6 +30,23 @@ $(document).ready(function() {
 			'border-color': $('.borderColor').val()
 		});
 	});
+	$('input[name="circle"]').click(function(){
+    		if ($(this).is(':checked')) {
+      			alert($(this).val());
+    		}
+	});
+/*
+$("label input[name='circle']").click(function() {
+
+	if ($(this).val('yes')) {
+		$('#' + map_link_ids[i]).css('border-radius', '100%');
+	} else {
+		$('#' + map_link_ids[i]).css('border-radius', '0');
+	}
+	
+	alert('clecked!');
+});
+*/
 });
 
 $('#map_link_name').keyup(function(){
@@ -284,24 +301,7 @@ function tab_select(event, ui) {
 	}
 }
 
-$('input[name="circle"]').click(function(){
-    if ($(this).is(':checked'))
-    {
-      alert($(this).val());
-    }
-  });
-/*
-$("label input[name='circle']").click(function() {
 
-	if ($(this).val('yes')) {
-		$('#' + map_link_ids[i]).css('border-radius', '100%');
-	} else {
-		$('#' + map_link_ids[i]).css('border-radius', '0');
-	}
-	
-	alert('clecked!');
-});
-*/
 
 if (!Array.prototype.indexOf) {
 	Array.prototype.indexOf = function(elt /*, from*/) {
