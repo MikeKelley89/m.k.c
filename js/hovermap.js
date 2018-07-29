f5dc63a5ab1b8148297a401e56c63f7bf953b27b
MikeKelley89 <mikekelley89@gmail.com>
2018-07-29 15:43:39 -0600

    Fix "space in name" for retrieving imgs and allow two hovermaps on same page
    Fix to work with spaces in path to image or image filename, line 13: encodeURI function.
    
    Fix to allow multiple hover maps on the same page, which I thought was supported, but I see now why it wasn't. Line 12: Was referencing $('.mapImage'), of which is the same class for every hover map on the page. I added the $(this).siblings() function so we could target only the .mapImage that is adjacent to the currently hovered-over hotspot.

--- js/hovermap.js
+++ js/hovermap.js
@@ -1,77 +1,79 @@
-/* <![CDATA[ */
+﻿/* <![CDATA[ */
 $(document).ready(function () {
-	'use strict';
+  'use strict';
 
-	$('.hoverMap').hover(function () {
-		$(this.id).add('.mapLink').addClass('disableStuff');
-	});
+  $('.hoverMap').hover(function () {
+    $(this.id).add('.mapLink').addClass('disableStuff');
+  });
 
-	$('.mapLink').hover(function () {
-		var bw = parseInt($(this).css('borderLeftWidth'), 10);
-		var position = $(this).position();
-		$(this).css({
-			'background-image': 'url(' + $('.mapImage').attr('src') + ')',
-			'background-position': 'top -' + (position.top + bw) + 'px' + ' ' + 'left -' + (position.left + bw) + 'px',
-			'background-repeat': 'no-repeat'
-		});
-	},
-	function () {
-		$(this).css({
-			'background-image': 'none'
-		});
-	});
+  $('.mapLink').hover(function () {
+    var bw = parseInt($(this).css('borderLeftWidth'), 10);
+    var position = $(this).position();
+    var url = $(this).siblings('.mapImage').attr('src');
+    url = encodeURI(url);
+    $(this).css({
+      'background-image': 'url(' + url + ')',
+      'background-position': 'top -' + (position.top + bw) + 'px' + ' ' + 'left -' + (position.left + bw) + 'px',
+      'background-repeat': 'no-repeat'
+    });
+  },
+  function () {
+    $(this).css({
+      'background-image': 'none'
+    });
+  });
 
-	$('.mapLink').each(function () {
-		var data = $(this).data();
-		var title = $(this).attr('href');
-		var target = 'false';
-		var offsetX = 0;
-		var offsetY = 0;
-		var my = 'bottom left';
-		var at = 'top right';
+  $('.mapLink').each(function () {
+    var data = $(this).data();
+    var title = $(this).attr('href');
+    var target = 'false';
+    var offsetX = 0;
+    var offsetY = 0;
+    var my = 'bottom left';
+    var at = 'top right';
 
-		if ($(this).attr('data-tooltip-atParent') === "true") {
-			target = $(this).parent();
-		}
+    if ($(this).attr('data-tooltip-atParent') === "true") {
+      target = $(this).parent();
+    }
 
-		$(this).qtip({
-			prerender: true,
-			style: {
-				tip: {
-					corner: false
-				},
-				classes: 'qtip-bootstrap popUp'
-			},
-			show: {
-				solo: true
-			},
-			position: {
-				viewport: $(window),
-				my: my,
-				at: at,
-				target: target,
-				adjust: {
-					method: 'shift',
-					x: offsetX,
-					y: offsetY
-				}
-			},
-			content: {
-				text: $('div' + title).html()
-			},
-			hide: {
-				fixed: true,
-				delay: 300
-			}
-		});
+    $(this).qtip({
+      prerender: true,
+      style: {
+        tip: {
+          corner: false
+        },
+        classes: 'qtip-bootstrap popUp'
+      },
+      show: {
+        solo: true
+      },
+      position: {
+        viewport: $(window),
+        my: my,
+        at: at,
+        target: target,
+        adjust: {
+          method: 'shift',
+          x: offsetX,
+          y: offsetY
+        }
+      },
+      content: {
+        text: $('div' + title).html()
+      },
+      hide: {
+        fixed: true,
+        delay: 300
+      }
+    });
 
-		for (var i in data) {
-			$(this).qtip('api').set(i, data[i]);
-		}
-	});
+    for (var i in data) {
+      $(this).qtip('api').set(i, data[i]);
+    }
+  });
 
-	$('.mapLink').click(function (event) {
-		event.preventDefault();
-	});
+  $('.mapLink').click(function (event) {
+    event.preventDefault();
+  });
 });
 /* ]]> */
